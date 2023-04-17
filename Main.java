dbc8b19fa3aa612b1abc98af4fe2901184002450
nawres-hamdi <126500642+nawres-hamdi@users.noreply.github.com>
2023-04-17 05:32:08 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+pipeline
+    agent any
+    stages {
+    stage('clone') {
+    steps {
+    sh "rm -rf *"
+    sh "git clone https://github.com/nawres-hamdi/mvn-helloworld"
     }
-}
+    }
+    stage('build') {
+    steps {
+    sh "cd jenkins-helloworld/ && javac Main.java"
+    }
+    }
+    stage('run') {
+    steps {
+    sh "cd jenkins-helloworld/ && java Main"
+    }
+    }
+    }
+    }
